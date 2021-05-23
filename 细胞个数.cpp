59020d525a77a837de2827ec525223d0103ee814
onion108 <57032603+onion108@users.noreply.github.com>
2021-05-23 17:45:49 +0800

    Update 细胞个数.cpp

--- 细胞个数.cpp
+++ 细胞个数.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
-#include<queue>
+// Oh...
+#include <iostream>
+#include <queue>
 using namespace std;
-struct node{
+struct node {
 	int x, y;
 };
 
@@ -26,31 +27,20 @@ int main(){
 			q.push((node){x0, y0});
 			cell[x0][y0] = '0';
 			while(!q.empty()){
-				cout << " 当前队首元素坐标： " << q.front().x << " " << q.front().y << ":\n";
 				for(int i = 0; i< 4; i++){
 					newNode.x = q.front().x + n1[i].x;
 					newNode.y = q.front().y + n1[i].y;
-					cout <<newNode.x << " " << newNode.y << ":";
 					if(newNode.x >= 0 && newNode.x < m
 			        && newNode.y >= 0 && newNode.y < n
 					&& cell[newNode.x][newNode.y] != '0') {
 						q.push(newNode);
 						cell[newNode.x][newNode.y] = '0';
-						cout << "入队";
-					} else cout << "不在范围或已经被吃掉"； 
-					cout << endl;
+					} 
 				}
 				q.pop();
-			}
-		    for(int i = 0; i < m; i++){
-				cout << cell[i] << endl;
-			}
-			
-			cout << "_______________________" << endl;     		
+			}	
 		}
 	}
     cout << count;
-    
-	
     return 0;
 }
